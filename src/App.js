a64206c8cb1aafd173810e2343af61cb640711b4
Tem-Tech <oyeyemileanne@gmail.com>
2022-02-03 10:47:40 +0000

    weather data api connected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./style.css";
+import SearchForm from "./search";
+import Heading from "./heading";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <br />
+        <Heading />
+        <SearchForm />
+        <br />
+
+        <div></div>
+        <hr />
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/Tem-Tech/weather-app-React"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Leanne Oyeyemi
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
